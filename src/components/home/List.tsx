2df21a030a78f95a22b21e8ca3ee2f1b06ceff0d
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-09-27 00:08:49 +0800

    更新实现

--- src/components/home/List.tsx
+++ src/components/home/List.tsx
@@ -1,58 +1,41 @@
 import PhotoAlbum from "react-photo-album";
 import display from "../photo/Origin";
 import './List.css'
+import { useEffect, useState } from "react";
+
+class ItemList {
+    src: string = ''
+    width: number = 0
+    height: number = 0
+}
 
 export const HomeList = () => {
-    const photos = [
-        {
-            src: '/examples/1.JPG',
-            width: 4672,
-            height: 3115,
-            href: ''
-        },
-        {
-            src: '/examples/2.JPG',
-            width: 6255,
-            height: 4170,
-            href: ''
-        },
-        {
-            src: '/examples/3.JPG',
-            width: 4417,
-            height: 6625,
-            href: ''
-        },
-        {
-            src: '/examples/4.JPG',
-            width: 7008,
-            height: 4672,
-            href: ''
-        },
-        {
-            src: '/examples/5.JPG',
-            width: 6332,
-            height: 4221,
-            href: ''
-        },
-        {
-            src: '/examples/6.JPG',
-            width: 3677,
-            height: 5515,
-            href: ''
-        }
-    ]
+    const [photoList, setPhotoList] = useState<ItemList[]>([])
+
+    useEffect(() => {
+        fetch('http://localhost:8190/list')
+            .then(res => res.json())
+            .then(res => {
+                for (let i = 0; i < res.length; i++) {
+                    res[i].src = 'http://localhost:8190' + res[i].src
+                }
+                setPhotoList(res)
+            })
+    }, [])
 
     const onClick = (index: number) => {
-        display(photos[index].src)
+        const src = photoList[index].src.replace('preview', 'origin')
+        console.log(src);
+        display(src)
     }
 
     return (
         <div>
-            <PhotoAlbum photos={photos} layout="masonry" spacing={0} padding={2}
+            <PhotoAlbum photos={photoList} layout="masonry" spacing={6} padding={0}
                 columns={5} onClick={({ index }) => {
                     onClick(index)
                 }} componentsProps={(_containerWidth) => ({
-                    imageProps: { style: { borderRadius: 10, boxShadow: '0 10px 10px rgba(0, 0, 0, 0.3)' } },
+                    imageProps: { loading: 'lazy', style: { borderRadius: 10, boxShadow: '0 10px 10px rgba(0, 0, 0, 0.3)' } },
                 })}></PhotoAlbum>
         </div>
     );
